f2ed3a89bd51bb3fd0de39081084eb2910cd5769
devpcmini <devpcmini10@gmail.com>
2023-04-11 21:53:24 +0900

    프로그래머스 평균 구하기 풀이

--- algorithm/src/main/java/com/algorithm/algorithm/programmers/level1/ToFinTheAve.java
+++ algorithm/src/main/java/com/algorithm/algorithm/programmers/level1/ToFinTheAve.java
@@ -2,7 +2,7 @@ package com.algorithm.algorithm.programmers.level1;
 
 public class ToFinTheAve {
 	public static void main(String[] args) {
-		int[] arr = {1,2,3,4};
+		int[] arr = {1,2,3,4}; 
 //		int[] arr = {5,5};
 		System.out.println(solution(arr));
 	}
