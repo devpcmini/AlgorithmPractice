c26373ea68966f45368a432c15d9550cfbf5c944
devpcmini <devpcmini10@gmail.com>
2023-04-22 12:21:23 +0900

    프로그래머스 행렬의 덧셈 풀이

---
+++ algorithm/src/main/java/com/algorithm/algorithm/programmers/level1/AddOfAMatrix.java
@@ -0,0 +1,22 @@
+package com.algorithm.algorithm.programmers.level1;
+
+import java.util.Arrays;
+
+public class AddOfAMatrix {
+	//행렬의 덧셈
+	public static void main(String[] args) {
+		int[][] arr1 = {{1,2},{2,3}};
+		int[][] arr2 = {{3,4},{5,6}};
+		System.out.println(Arrays.toString(solution(arr1,arr2)));
+	}
+	
+	public static int[][] solution(int[][] arr1, int[][] arr2){
+		int[][] answer = new int[arr1.length][arr1[0].length];
+		for(int i=0; i<arr1.length; i++) {
+			for(int j=0; j<arr1[i].length; j++) {
+				answer[i][j] = arr1[i][j] + arr2[i][j];
+			}
+		}
+		return answer;
+	}
+}
