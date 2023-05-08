4d2b367c8cbdc312385e98257861e049e0869e41
devpcmini <devpcmini10@gmail.com>
2023-05-09 00:17:54 +0900

    백준 AxB 풀이

---
+++ algorithm/src/main/java/com/algorithm/algorithm/baekjoon/Quiz_10998.java
@@ -0,0 +1,17 @@
+package com.algorithm.algorithm.baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+//AxB
+public class Quiz_10998 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int A = Integer.parseInt(st.nextToken());
+		int B = Integer.parseInt(st.nextToken());
+		System.out.println(A*B);
+	}
+}
