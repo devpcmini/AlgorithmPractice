ca7ef7398633359ad2503cef70a5a6b1944b2692
devpcmini <devpcmini10@gmail.com>
2023-05-12 01:10:02 +0900

    백준 ??! 풀이

---
+++ algorithm/src/main/java/com/algorithm/algorithm/baekjoon/Quiz_10926.java
@@ -0,0 +1,13 @@
+package com.algorithm.algorithm.baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+//??!
+public class Quiz_10926 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		System.out.println(br.readLine() + "??!");
+	}
+}
