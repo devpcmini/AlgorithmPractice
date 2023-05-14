05fc6b919b9fd40b096be0438ce886bb4312cd2e
devpcmini <devpcmini10@gmail.com>
2023-05-14 14:25:45 +0900

    백준 고양이 풀이

---
+++ algorithm/src/main/java/com/algorithm/algorithm/baekjoon/Quiz_10171.java
@@ -0,0 +1,11 @@
+package com.algorithm.algorithm.baekjoon;
+
+//고양이
+public class Quiz_10171 {
+	public static void main(String[] args) {
+		System.out.println("\\    /\\");
+        System.out.println(" )  ( ')");
+        System.out.println("(  /  )");
+        System.out.println(" \\(__) |");
+	}
+}
