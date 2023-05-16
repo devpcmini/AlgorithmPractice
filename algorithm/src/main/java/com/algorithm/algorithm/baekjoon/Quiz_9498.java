0203d91099e1b4f541163b59a5a23de909882ecf
devpcmini <devpcmini10@gmail.com>
2023-05-17 02:25:26 +0900

    백준 시험성적 풀이

---
+++ algorithm/src/main/java/com/algorithm/algorithm/baekjoon/Quiz_9498.java
@@ -0,0 +1,24 @@
+package com.algorithm.algorithm.baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+//시험 성적
+public class Quiz_9498 {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int score = Integer.parseInt(br.readLine());
+		if(score >= 90 && score <= 100) {
+			System.out.println("A");
+		} else if(score >= 80 && score <= 89) {
+			System.out.println("B");
+		} else if(score >= 70 && score <= 79) {
+			System.out.println("C");
+		} else if(score >= 60 && score <= 69) {
+			System.out.println("D");
+		} else {
+			System.out.println("F");
+		}
+	}
+}
