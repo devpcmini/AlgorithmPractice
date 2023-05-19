07f7181a87eee2f476c53b8685c757b4db5d0c2a
devpcmini <devpcmini10@gmail.com>
2023-05-20 00:53:14 +0900

    백준 A+B -3 풀이

---
+++ algorithm/src/main/java/com/algorithm/algorithm/baekjoon/Quiz_10950.java
@@ -0,0 +1,20 @@
+package com.algorithm.algorithm.baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+//A+B -3
+public class Quiz_10950 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		for(int i=0; i<N; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int A = Integer.parseInt(st.nextToken());
+			int B = Integer.parseInt(st.nextToken());
+			System.out.println(A + B);
+		}
+	}
+}
