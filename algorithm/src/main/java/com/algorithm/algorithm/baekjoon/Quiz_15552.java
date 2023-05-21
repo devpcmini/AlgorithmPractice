ea0d7db3406a259dcd2c9d2047906b3594109b24
devpcmini <devpcmini10@gmail.com>
2023-05-22 00:16:35 +0900

    백준 빠른 A+B 풀이

---
+++ algorithm/src/main/java/com/algorithm/algorithm/baekjoon/Quiz_15552.java
@@ -0,0 +1,24 @@
+package com.algorithm.algorithm.baekjoon;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+//빠른 A+B
+public class Quiz_15552 {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		int T = Integer.parseInt(br.readLine());
+		for(int i=0; i<T; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int A = Integer.parseInt(st.nextToken());
+			int B = Integer.parseInt(st.nextToken());
+			bw.write(A + B + "\n");
+		}
+		bw.close();
+	}
+}
