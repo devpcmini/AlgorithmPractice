9e6e5d8eabc8cccb0f498d0e43b295d7c950e00d
devpcmini <devpcmini10@gmail.com>
2023-05-23 00:04:10 +0900

    백준 별찍기 - 2 풀이

---
+++ algorithm/src/main/java/com/algorithm/algorithm/baekjoon/Quiz_2439.java
@@ -0,0 +1,26 @@
+package com.algorithm.algorithm.baekjoon;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+//별찍기 - 2
+public class Quiz_2439 {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		int N = Integer.parseInt(br.readLine());
+		for(int i=1; i<=N; i++) {
+			for(int j=0; j<N - i; j++) {
+				bw.write(" ");
+			}
+			for(int j=0; j<i; j++) {
+				bw.write("*");
+			}
+			bw.write("\n");
+		}
+		bw.close();
+	}
+}
