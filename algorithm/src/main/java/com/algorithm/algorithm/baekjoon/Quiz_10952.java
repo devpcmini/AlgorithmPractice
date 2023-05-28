319129ce08041edf9a4297776753aa58633a1245
devpcmini <devpcmini10@gmail.com>
2023-05-29 00:06:59 +0900

    백준 A+B - 5 풀이

---
+++ algorithm/src/main/java/com/algorithm/algorithm/baekjoon/Quiz_10952.java
@@ -0,0 +1,26 @@
+package com.algorithm.algorithm.baekjoon;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+//A+B - 5
+public class Quiz_10952 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		while(true) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int A = Integer.parseInt(st.nextToken());
+			int B = Integer.parseInt(st.nextToken());
+			if(A == 0 && B == 0) {
+				break;
+			}
+			bw.write((A+B) + "\n");
+		}
+		bw.close();
+	}
+}
