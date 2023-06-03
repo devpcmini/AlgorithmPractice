c675fb891014b313f06bee682318bbf83454c47d
devpcmini <devpcmini10@gmail.com>
2023-06-04 00:25:23 +0900

    백준 그대로 출력하기 풀이

---
+++ algorithm/src/main/java/com/algorithm/algorithm/baekjoon/Quiz_11718.java
@@ -0,0 +1,16 @@
+package com.algorithm.algorithm.baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+//그대로 출력하기
+public class Quiz_11718 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String str = "";
+		while((str = br.readLine())!= null) {
+			System.out.println(str);
+		}
+	}
+}
