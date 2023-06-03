5a660ba8424bc4fdb3fe7d2c143c4e4aa139028c
devpcmini <devpcmini10@gmail.com>
2023-06-04 00:25:00 +0900

    백준 새싹 풀이

---
+++ algorithm/src/main/java/com/algorithm/algorithm/baekjoon/Quiz_25083.java
@@ -0,0 +1,14 @@
+package com.algorithm.algorithm.baekjoon;
+
+//새싹
+public class Quiz_25083 {
+	public static void main(String[] args) {
+		
+		System.out.println("         ,r'\"7");
+		System.out.println("r`-_   ,'  ,/");
+		System.out.println(" \\. \". L_r'");
+		System.out.println("   `~\\/");
+		System.out.println("      |");
+		System.out.println("      |");
+	}
+}
