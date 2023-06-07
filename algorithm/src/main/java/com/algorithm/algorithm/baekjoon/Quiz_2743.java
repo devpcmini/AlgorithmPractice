d30ea82ef5b8165a05d817de054734b53e978d41
devpcmini <devpcmini10@gmail.com>
2023-06-08 00:02:23 +0900

    백준 단어 길이 재기 풀이

---
+++ algorithm/src/main/java/com/algorithm/algorithm/baekjoon/Quiz_2743.java
@@ -0,0 +1,14 @@
+package com.algorithm.algorithm.baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+//단어 길이 재기
+public class Quiz_2743 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String str = br.readLine();
+		System.out.println(str.length());
+	}
+}
