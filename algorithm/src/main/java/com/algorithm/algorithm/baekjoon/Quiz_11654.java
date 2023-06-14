ffddbfe313795a98cc04ac7ce9066c2e484e1654
devpcmini <devpcmini10@gmail.com>
2023-06-14 22:19:22 +0900

    백준 아스키 코드 풀이

---
+++ algorithm/src/main/java/com/algorithm/algorithm/baekjoon/Quiz_11654.java
@@ -0,0 +1,13 @@
+package com.algorithm.algorithm.baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+//아스키 코드
+public class Quiz_11654 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		System.out.println((int)br.readLine().charAt(0));
+	}
+}
