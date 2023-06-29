d7841904a37c3b5053a074b4932f17c105f39b9d
devpcmini <devpcmini10@gmail.com>
2023-06-30 00:47:18 +0900

    백준 달팽이는 올라가고 싶다. 풀이

---
+++ algorithm/src/main/java/com/algorithm/algorithm/baekjoon/Quiz_2869.java
@@ -0,0 +1,18 @@
+package com.algorithm.algorithm.baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+//달팽이는 올라가고 싶다.
+public class Quiz_2869 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int A = Integer.parseInt(st.nextToken());
+		int B = Integer.parseInt(st.nextToken());
+		int V = Integer.parseInt(st.nextToken());
+		System.out.println(((V-B-1) / (A-B)) + 1);
+	}	
+}
